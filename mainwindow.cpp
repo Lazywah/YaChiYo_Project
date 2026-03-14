43706344cee90ebb6ae8bc99f3df5de5a243e8c5
NyaLazy <nyalazyforwork@gmail.com>
2026-03-14 19:29:07 +0800

    feat: 設置無邊框半透明視窗 & 新增右鍵選單(關閉桌寵) & 滑鼠拖曳事件

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,61 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QMenu>
+#include <QAction>
+#include <QContextMenuEvent>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // --> setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
+    Qt::WindowFlags flags = windowFlags();
+
+    flags |= Qt::FramelessWindowHint;   // ZH: 消除視窗標題與邊框 | EN: Remove window title and border
+    flags |= Qt::WindowStaysOnTopHint;  // ZH: 使視窗永遠置於頂層 | EN: Make window always stay on top
+
+    setWindowFlags(flags);
+
+    setAttribute(Qt::WA_TranslucentBackground); // ZH: 將視窗背景設為透明 | EN: Set window background to transparent
+}
+
+// ZH: 宣告右鍵選單中的"關閉桌寵"事件 | EN: Declare event "Close Desktop Pet" in right-click menu
+void MainWindow::contextMenuEvent(QContextMenuEvent *event)
+{
+    QMenu menu(this);
+    QAction *closeAction = menu.addAction("關閉桌寵");
+
+    connect(closeAction, &QAction::triggered, this, &MainWindow::close);
+
+    menu.exec(event->globalPos());
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton)
+    {
+        // ZH: 紀錄滑鼠點擊位置與視窗左上角的偏差值 | EN: record offset mouse click position to window Top Left
+        m_offset = event->globalPosition().toPoint() - this->pos();
+        event->accept();
+    }
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    if (event->buttons() & Qt::LeftButton)
+    {
+        // ZH: 移動視窗到目前滑鼠位置-偏差值 (絕對位置) | EN: move window to current mouse position - offset (absolute position)
+        move(event->globalPosition().toPoint() - m_offset);
+        event->accept();
+    }
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *event)
+{
+    Q_UNUSED(event);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPoint>               // ZH: 紀錄點擊座標 | EN: Record click coordinates
+#include <QMouseEvent>          // ZH: 處理滑鼠事件 | EN: Handling mouse events
+#include <QContextMenuEvent>    // ZH: 處理右鍵選單 | EN: Handling right-click menus
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +20,19 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    // ZH: 宣告右鍵選單事件 | EN: Declare right-click menu event
+    void contextMenuEvent(QContextMenuEvent *event) override;
+
+    // ZH: 宣告滑鼠拖曳相關事件 | EN: Declare mouse dragging event
+    void mousePressEvent(QMouseEvent * event) override;
+    void mouseMoveEvent(QMouseEvent * event) override;
+    void mouseReleaseEvent(QMouseEvent * event) override;
+
 private:
     Ui::MainWindow *ui;
+
+    // ZH: 用於計算滑鼠與視窗左上角的偏差值 | EN: Used to calculate the offset between the mouse and the top left corner of the viewport
+    QPoint m_offset;
 };
 #endif // MAINWINDOW_H
