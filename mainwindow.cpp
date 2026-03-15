912cd9a39b949b0672b3166b4871cf3d13c08948
NyaLazy <nyalazyforwork@gmail.com>
2026-03-15 23:23:25 +0800

    feat: 新增狀態管理 & 物理引擎(站立、被捕捉)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
-#include <QMenu>
-#include <QAction>
-#include <QContextMenuEvent>
+#include <QMenu>                // ZH: 右鍵選單容器 | EN: Right-clock menu container
+#include <QAction>              // ZH: 選單動作項目 | EN: Menu actions
+#include <QContextMenuEvent>    // ZH: 右鍵點擊事件 | EN: Right-click event
 #include <QPixmap>              // ZH: 處理靜態圖 | EN: Processing static images
 #include <QMovie>               // ZH: 處理動態圖 | EN: Processing dynamic images
+#include <QScreen>              // ZH: 獲取螢幕資訊 | EN: Get screen information
+#include <QGuiApplication>      // ZH: 監聽螢幕變化 | EN: Listen to screen changes
+#include <QTimer>
+
+//===============================================================================================
+
+//===============================================================================================
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -15,18 +22,21 @@ MainWindow::MainWindow(QWidget *parent)
 
     // --> setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
     Qt::WindowFlags flags = windowFlags();
-
     flags |= Qt::FramelessWindowHint;   // ZH: 消除視窗標題與邊框 | EN: Remove window title and border
     flags |= Qt::WindowStaysOnTopHint;  // ZH: 使視窗永遠置於頂層 | EN: Make window always stay on top
-
     setWindowFlags(flags);
 
     setAttribute(Qt::WA_TranslucentBackground); // ZH: 將視窗背景設為透明 | EN: Set window background to transparent
 
-    loadImage("normal.jpg");    // ZH: 載入圖片 | EN: load image
-
-    //loadAnimation("normal.gif");  // ZH: 載入動畫 | EN: load animation
+    physicsTimer = new QTimer(this);                                            // ZH: 初始化物理引擎計時器 | EN: Initialize physics engine timer
+    connect(physicsTimer, &QTimer::timeout, this, &MainWindow::timerProcess);   // ZH: 串接上發送者、訊號、接收者、需運行函數 | EN: Connect the sender, signal, receiver, and function to be executed.
+    physicsTimer->start(16);                                                    // ZH: 啟用物理引擎計時器，更新頻率 16ms(約 60FPS) | EN: Enable physics engine timer, updating at a frequency of 16ms(approximately 60 FPS)
+    currentState = Standing;
 }
+
+//===============================================================================================
+
+//===============================================================================================
 
 // ZH: 宣告右鍵選單中的"關閉桌寵"事件 | EN: Declare event "Close Desktop Pet" in right-click menu
 void MainWindow::contextMenuEvent(QContextMenuEvent *event)
@@ -43,10 +53,7 @@ void MainWindow::mousePressEvent(QMouseEvent *event)
 {
     if (event->button() == Qt::LeftButton)
     {
-        // ZH: 點擊切換圖片 | EN: Click to switch image
-        loadImage("catch.png");
-        // ZH: 點擊切換動畫 | EN: Click to switch animation
-        //loadAnimation("catch.png");
+        currentState = Captured;
 
         // ZH: 滑鼠拖曳功能(紀錄初始點) | EN: Mouse drag function (record initial point)
         m_offset = event->globalPosition().toPoint() - this->pos(); // ZH: 紀錄滑鼠點擊位置與視窗左上角的偏差值 | EN: record offset mouse click position to window Top Left
@@ -68,10 +75,8 @@ void MainWindow::mouseReleaseEvent(QMouseEvent *event)
 {
     if (event->button() == Qt::LeftButton)
     {
-        // ZH: 點擊切換圖片 | EN: Click to switch image
-        loadImage("normal.jpg");
-        // ZH: 點擊切換動畫 | EN: Click to switch animation
-        //loadAnimation("catch.png");
+        currentState = Standing;
+        physicsTimer->start(16);    // ZH: 啟用物理引擎計時器，更新頻率 16ms(約 60FPS) | EN: Enable physics engine timer, updating at a frequency of 16ms(approximately 60 FPS)
     }
 
     //Q_UNUSED(event);    // ZH: 僅用於消除因為撰寫函數邏輯的警告 | EN: Used only to eliminate warnings caused by writing function logic
@@ -109,6 +114,105 @@ void MainWindow::loadAnimation(QString filename)
 
     movie->start();
 }
+
+void MainWindow::timerProcess()
+{
+    updatePhysics();
+    setState();
+}
+
+void MainWindow::setState()
+{
+    if (currentState == Standing)
+    {
+        loadImage("normal.jpg");
+        //loadAnimation("normal.gif");
+    }
+    else if (currentState == Flying)
+    {
+        // ID: feat-1
+        // ZH: 未來新增功能 | EN: Future new features
+    }
+    else if (currentState == Hovering)
+    {
+        // ID: feat-2
+        // ZH: 未來新增功能 | EN: Future new features
+    }
+    else if (currentState == Captured)
+    {
+        physicsTimer->stop();   // ZH: 停用物理引擎計時器 | EN: Disable physics engine timer
+
+        loadImage("catch.png");
+        //loadAnimation("catch.png");
+    }
+}
+
+void MainWindow::updatePhysics()
+{
+    switch (currentState)
+    {
+        case Standing:
+            // ZH: 啟用重力使桌寵落地 | EN: Use gravity to make desktop pet land
+            applyGravity();
+            checkGroundCollision();
+            break;
+
+        case Flying:
+            // ID: feat-1
+            // ZH: 未來新增功能 | EN: Future new features
+            // ZH: 關閉重力，執行位移邏輯，使桌寵前往滑鼠位置 | EN: Turn off gravity, execute displacement logic, and move the desktop pet to the mouse position
+            //moveToTarget();
+            break;
+
+        case Hovering:
+            // ID: feat-2
+            // ZH: 未來新增功能 | EN: Future new features
+            // ZH: 垂直速度設為 0，只做微小的上下浮動 (Sin Wave) | EN: The vertical velocity is set to 0, resulting in only a slight up-and-down fluctuation (Sin Wave)
+            //applyBovverEffect();
+            break;
+
+        case Captured:
+            break;
+    }
+}
+
+void MainWindow::applyGravity()
+{
+    // ZH: 未落地則加速 | EN: Accelerate before landing
+    if (!isGrounded)
+    {
+        velocityY += gravity;
+    }
+
+    // ID: feat-3
+    // ZH: 未來可新增 X 軸慣性 | EN: Future additions could include X-axis inertia.
+    // ZH: 不移動 X 座標，僅變更 Y 座標 | EN: Without moving the X coordinate, only change the Y coordinate
+    this->move(this->x(), this->y() + (int)velocityY);
+}
+
+void MainWindow::checkGroundCollision()
+{
+    // ZH: 獲取當前視窗所在螢幕的可用區域(避開工作列) | EN: Get the available area of ​​the screen where the current window is located (excluding the task bar)
+    QRect screenRect = QGuiApplication::primaryScreen()->availableGeometry();
+    // ZH: 計算地面的 Y 座標(螢幕底部-視窗高度) | EN: Calculate the Y coordinates of the ground (bottom of screen - viewport height)
+    int groundY = screenRect.bottom() - this->height();
+
+    // ZH: 碰撞檢測邏輯 | EN: Collision detection logic
+    if (this->y() >= groundY)   // ZH: 已經到達或超過地面 | EN: Have reached or exceeded the ground
+    {
+        this->move(this->x(), groundY); // ZH: 強制對齊地板 | EN: Forced Alignment Floor
+        velocityY = 0;                  // ZH: 速度歸零 | EN: Speed ​​to zero
+        isGrounded = true;              // ZH: 標記為落地狀態 | EN: Marked as landed
+    }
+    else    // ZH: 未到達地面 | EN: Not reached the ground
+    {
+        isGrounded = false;
+    }
+}
+
+//===============================================================================================
+
+//===============================================================================================
 
 MainWindow::~MainWindow()
 {
--- mainwindow.h
+++ mainwindow.h
@@ -12,6 +12,8 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
+class QTimer;
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -19,6 +21,23 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    // ZH: 桌寵狀態(事件)列表 | EN: Desktop pet status(event) list
+    enum State { Standing, Flying, Hovering, Captured };
+    // ZH: 圖片 & 動畫載入路徑 | EN: Image & animation load path
+    const QString imagePath = ":/res/images/characterAnimation/";
+    // ZH: 用於計算滑鼠與視窗左上角的偏差值 | EN: Used to calculate the offset between the mouse and the top left corner of the viewport
+    QPoint m_offset;
+    // ZH: 物理引擎變數與函數 | EN: Physics engine value and function
+    QTimer *physicsTimer;
+    double velocityY = 0;           // ZH: 垂直速度 | EN: Vertical velocity
+    const double gravity = 0.8;     // ZH: 重力加速度 | EN: Gravitational acceleration
+    bool isGrounded = false;        // ZH: 在地面上(狀態) | EN: on the ground(status)
+    State currentState = Standing;  // ZH: 當前狀態 | EN: Current state
+    void applyGravity();            // ZH: 計算下落位移 | EN: Calculate the falling displacement
+    void checkGroundCollision();    // ZH: 螢幕底部偵測 | EN: Bottom Screen Detection
 
 protected:
     // ZH: 宣告右鍵選單事件 | EN: Declare right-click menu event
@@ -29,15 +48,15 @@ protected:
     void mouseMoveEvent(QMouseEvent * event) override;
     void mouseReleaseEvent(QMouseEvent * event) override;
 
-    // ZH: 圖片 & 動畫載入流程 | EN: Image & Animation Loading Process
+    // ZH: 圖片 & 動畫載入流程 | EN: Image & Animation loading process
     void loadImage(QString filename);
     void loadAnimation(QString filename);
 
-private:
-    Ui::MainWindow *ui;
-    // ZH: 圖片 & 動畫載入路徑 | EN: Image & animation load path
-    const QString imagePath = ":/res/images/characterAnimation/";
-    // ZH: 用於計算滑鼠與視窗左上角的偏差值 | EN: Used to calculate the offset between the mouse and the top left corner of the viewport
-    QPoint m_offset;
+    // ZH: 時間刻內流程 | EN: Time-bound process
+    void timerProcess();
+    // ZH: 狀態設定流程 | EN: Status setting process
+    void setState();
+    // ZH: 物理引擎流程 | EN: Physics engine process
+    void updatePhysics();
 };
 #endif // MAINWINDOW_H
